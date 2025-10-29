1b930698309a4fb19b860de607214f862f458317
WisnuIbnu <wisnuiben21@gmail.com>
2025-10-29 10:38:37 +0700

    add one of workspace controller: create new workspace

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,6 +14,7 @@ import authRoutes from "./routes/auth.route";
 import "./config/passport.config";
 import userRoutes from "./routes/user.route";
 import isAuthenticated from "./middlewares/isAuthenticated.middleware";
+import workspaceRoutes from "./routes/workspace.route";
 
 const app = express();
 const BASE_PATH = config.BASE_PATH;
@@ -52,6 +53,7 @@ app.get('/', asyncHandler( async(req: Request, res: Response, next: NextFunction
 
 app.use(`${BASE_PATH}/auth`, authRoutes);
 app.use(`${BASE_PATH}/user`, isAuthenticated ,userRoutes);
+app.use(`${BASE_PATH}/workspace`, isAuthenticated ,workspaceRoutes);
 
 app.use(errorHandler);
 
---
+++ backend/src/validation/workspace.validation.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+
+export const nameSchema = z.string().trim().min(1, { message: "Name is required "}).max(255);
+
+export const descriptionSchema = z.string().trim().optional();
+
+export const createWorkspaceSchema = z.object({
+  name: nameSchema,
+  descriptioin: descriptionSchema,
+});
+
+export const updateWorkspaceSchema = z.object({
+  name: nameSchema,
+    descriptioin: descriptionSchema,
+});
