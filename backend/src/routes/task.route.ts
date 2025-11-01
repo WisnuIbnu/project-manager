68121346248bc28b9982f0c38b2f9f1f5f6ef99f
WisnuIbnu <wisnuiben21@gmail.com>
2025-11-01 15:27:26 +0700

    add one of task controller: get all of task by workspaceId and filters

--- backend/src/routes/task.route.ts
+++ backend/src/routes/task.route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createTaskController, deleteTaskController, updateTaskController } from "../controllers/task.controller";
+import { createTaskController, deleteTaskController, getAllTasksController, updateTaskController } from "../controllers/task.controller";
 
 const taskRoutes = Router();
 
 taskRoutes.post("/project/:projectId/workspace/:workspaceId/create", createTaskController);
 taskRoutes.put("/:id/project/:projectId/workspace/:workspaceId/update", updateTaskController);
+taskRoutes.get("/workspace/:workspaceId/all", getAllTasksController);
 taskRoutes.delete("/:id/project/:projectId/workspace/:workspaceId/delete", deleteTaskController);
 
 export default taskRoutes;
